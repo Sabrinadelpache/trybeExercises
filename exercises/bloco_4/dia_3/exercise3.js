a94c8443e15fbf84a76a5e53b8d6bd8d40d85ae0
Sabrina A <sasa2605.alves@gmail.com>
2021-01-04 09:51:38 -0300

    Atualização bloco 4

--- exercises/bloco_4/dia_3/exercise3.js
+++ exercises/bloco_4/dia_3/exercise3.js
@@ -2,7 +2,10 @@ function asterisk(n) {
     if (n > 1) {
         for (let i = 0; i < n; i += 1) {
             let symbol = '';
-            for (let j = 0; j < n; j += 1) {
+            for(let k = n; k <= n; k -= 1) {
+                symbol += ' ';
+            }
+            for (let j = 0; j <= i; j += 1) {
                 symbol += '*';
             }
             console.log(symbol);
