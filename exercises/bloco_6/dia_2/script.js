f4c6a7917760972e8ce5ea9235651d1eb9f0632a
Sabrina A <sasa2605.alves@gmail.com>
2021-01-08 17:47:37 -0300

    6.2 exercise done

--- exercises/bloco_6/dia_2/script.js
+++ exercises/bloco_6/dia_2/script.js
@@ -1,20 +1,10 @@
-let dataInicio = document.getElementById('data').value;
-let chama = document.getElementById('data')
-
-function data () {
-    for (let index = 0; index < dataInicio; index += 1) {
-        let diaMes = dataInicio[index] + dataInicio[index + 1];
-        console.log(diaMes);
-    }
-    
-}
-chama.addEventListener('mouseout', data)
-window.onload = function() {
+window.onload = function () {
     let selectFather = document.getElementById('states');
 
-    function statesCreation () {
-        let stateOptions = ['Selecione seu estado', 'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF']    
+    function statesCreation() {
+        let stateOptions = ['Selecione seu estado', 'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF']
         for (let i = 0; i < stateOptions.length; i += 1) {
+            /* Usar o disabled????*/
             let state = document.createElement('option');
             state.textContent = stateOptions[i]
             selectFather.appendChild(state);
@@ -22,33 +12,35 @@ window.onload = function() {
     }
     statesCreation();
 }
+let datePicker = document.getElementById('datepicker');
+var picker = new Pikaday ({
+    field : document.getElementById('datepicker'),
+    firstDay : 1,
+    minDate : new Date(0000, 0, 1),
+    maxDate : new Date(2021, 0, 7),
+    yearRange : [0000,2021]
+})
 
-let inputData = document.getElementById('data');
-function date() {
-    let valueData = inputData.value;
-    let day = '';
-    let month = '';
-    let year = '';
-    for (let i = 0; i < valueData.length; i += 1) {
-        if ((i == 0) || (i == 1) ) {
-            day += valueData[i];
-        }   
-            else if ((i == 2) || ( i == 3)) {
-                month += valueData[i];
-            }
-                else {
-                    year += valueData[i];
-                }
+const validation = new JustValidate('#validationForm', {
+    rules : {
+        number: {
+            maxLength: 11,
+            required: true
+        },
+        textCity: {
+            maxLength: 28,
+            required: true
+        },
+        checkbox: {
+            required: true
+        },
+        text1: {
+            maxLength: 1000,
+            required : true
+        },
+        text2: {
+            maxLength: 500,
+            required: true
+        }
     }
-
-    if ((day <= '00') || (day > '31')) {
-        alert('Data inválida! Por favor, preencha a data com valores entre 01 e 31.')
-    }
-    if ((month <= '00') || (month > '12')) {
-        alert('Mês inválido! Por favor, preencha os mês com valores válidos entre 01 e 12.')
-    }
-    if (year <= 0) {
-        alert('O ano é inválido! Por favor preencha um ano acima de 0.');
-    }
-}
-inputData.addEventListener('focusout', date)
+});
